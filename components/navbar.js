5ac25525bb8011e029d165935871c2f983e34e79
Salmaan Saeed <salsaeed999@gmail.com>
2023-03-24 14:49:41 -0400

    adjusted the top padding of the main tag

--- components/navbar.js
+++ components/navbar.js
@@ -12,7 +12,7 @@ function BrandExample() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="#link">Link</Nav.Link>
             <NavDropdown
               id="nav-dropdown-dark-example"
